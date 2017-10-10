04331e9967524405b81cff51fc176059a30dd16f
Doug Adams <douglas.adams@me.com>
2017-10-10 11:24:44 -0400

    Initial Commit

---
+++ Waterfall/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Waterfall
+//
+//  Created by Douglas Adams on 10/10/17.
+//  Copyright © 2017 Douglas Adams. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Waterfall/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Waterfall
+//
+//  Created by Douglas Adams on 10/10/17.
+//  Copyright © 2017 Douglas Adams. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
